da156e0b9b33be8e729fae156ce4c13e98184dda
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-06-12 16:28:20 -0600

    added serialization, reading/writing

--- app/src/main/java/edu/cnm/deepdive/atthemovies/MoviesFragment.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/MoviesFragment.java
@@ -4,7 +4,6 @@ package edu.cnm.deepdive.atthemovies;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,12 +14,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 import edu.cnm.deepdive.atthemovies.model.Movie;
 import edu.cnm.deepdive.atthemovies.viewmodel.MoviesViewModel;
 import java.util.ArrayList;
-
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -45,13 +45,33 @@ private Context context;
 
     final View view = inflater.inflate(R.layout.fragment_movies, container, false);
 
-    ListView moviesListView = view.findViewById(R.id.movies_list);
+    final ListView moviesListView = view.findViewById(R.id.movies_list);
 
     final MoviesViewModel viewModel = ViewModelProviders.of(getActivity()).get(MoviesViewModel.class);
 
-    final ArrayAdapter<Movie> adapter = new ArrayAdapter<Movie>(context, android.R.layout.simple_list_item_1,
-        viewModel.getMovies());
-    moviesListView.setAdapter(adapter);
+
+    viewModel.getMoviesLiveData(context).observe(this, new Observer<Map<Long, Movie>>() {
+      @Override
+      public void onChanged(Map<Long, Movie> longMovieMap) {
+        final ArrayAdapter<Movie> adapter = new ArrayAdapter<Movie>(context, android.R.layout.simple_list_item_1,
+            new ArrayList<Movie>(longMovieMap.values()));
+        moviesListView.setAdapter(adapter);
+        moviesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          @Override
+          public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //This code opens the actor fragment and passes the movie id that was clicked
+            MoviesFragmentDirections.ActionMoviesFragmentToActorsFragment action =
+                MoviesFragmentDirections.actionMoviesFragmentToActorsFragment()
+                    .setMovieId(adapter.getItem(position).getId());
+            Navigation.findNavController(view).navigate(action);
+
+
+          }
+        });
+      }
+
+    });
+
 
     final Spinner genreSpinner = view.findViewById(R.id.new_movie_genre);
     ArrayAdapter<Movie.Genre> genreAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, Movie.Genre.values());
@@ -67,25 +87,12 @@ private Context context;
         newMovie.setTitle(newMovieNameEditText.getText().toString());
         newMovie.setScreenwriter(newMovieScreenwriter.getText().toString());
         newMovie.setGenre((Movie.Genre) genreSpinner.getSelectedItem());
-        viewModel.addMovie(newMovie);
-        adapter.clear();
-        adapter.addAll(viewModel.getMovies());
+        viewModel.addMovie(newMovie, context);
         newMovieNameEditText.setText("");
       }
     });
 
-    moviesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-      @Override
-      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        //This code opens the actor fragment and passes the movie id that was clicked
-        MoviesFragmentDirections.ActionMoviesFragmentToActorsFragment action =
-            MoviesFragmentDirections.actionMoviesFragmentToActorsFragment()
-                .setMovieId(adapter.getItem(position).getId());
-        Navigation.findNavController(view).navigate(action);
 
-
-      }
-    });
     // Inflate the layout for this fragment
 
     return view;
