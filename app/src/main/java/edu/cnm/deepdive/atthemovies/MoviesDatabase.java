fa952ffbd1b76137cda4e27a871fafda61fee356
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-06-26 14:10:33 -0600

    Added ability to populate database.

--- app/src/main/java/edu/cnm/deepdive/atthemovies/MoviesDatabase.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/MoviesDatabase.java
@@ -1,9 +1,12 @@
 package edu.cnm.deepdive.atthemovies;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 import edu.cnm.deepdive.atthemovies.model.Actor;
 import edu.cnm.deepdive.atthemovies.model.ActorMovieJoin;
 import edu.cnm.deepdive.atthemovies.model.Movie;
@@ -14,21 +17,53 @@ import edu.cnm.deepdive.atthemovies.model.dao.MovieDao;
 @Database(entities = {Movie.class, Actor.class, ActorMovieJoin.class}, version = 1)
 public abstract class MoviesDatabase extends RoomDatabase {
 
+  private static MoviesDatabase INSTANCE;
+
+  public static MoviesDatabase getInstance(Context context) {
+    if (INSTANCE == null){
+      synchronized (MoviesDatabase.class) {
+        if (INSTANCE == null) {
+          INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+              MoviesDatabase.class, "movies_room")
+              .fallbackToDestructiveMigration()
+              .addCallback(new Callback() {
+                @Override
+                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                  super.onCreate(db);
+                  new PopulateDbTask(INSTANCE).execute();
+                }
+              })
+              .build();
+        }
+      }
+    }
+    return INSTANCE;
+
+  }
+
   public abstract MovieDao movieDao();
 
   public abstract ActorDao actorDao();
 
   public abstract ActorMovieJoinDao actorMovieJoinDao();
 
-  private static MoviesDatabase INSTANCE;
+  private static class PopulateDbTask extends AsyncTask<Void, Void, Void> {
 
-  public static MoviesDatabase getInstance(Context context) {
-    if (INSTANCE == null){
-      INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-          MoviesDatabase.class, "movies_room").build();
+    private final MoviesDatabase db;
+
+    PopulateDbTask(MoviesDatabase db) {
+      this.db = db;
     }
-    return INSTANCE;
 
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+      Movie avengers = new Movie();
+      avengers.setTitle("Avengers");
+      avengers.setGenre(Movie.Genre.ACTION);
+      avengers.setScreenwriter("Joss Whedon");
+      db.movieDao().insert(avengers);
+      return null;
+    }
   }
-
 }
